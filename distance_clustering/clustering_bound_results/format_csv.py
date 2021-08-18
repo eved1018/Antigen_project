1cac3a683eb95622f7a499220c01c6abc5bda8c8
mcarroll8 <moshecarroll8@gmail.com>
2021-08-18 15:56:31 -0400

    commit

--- distance_clustering/clustering_bound_results/format_csv.py
+++ distance_clustering/clustering_bound_results/format_csv.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
-with open("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/bound_compiled_2.csv") as infile:
+with open("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/bound_fscore_compiled_2.csv") as infile:
     for line in infile:
         protein_infile = line.strip().split(",")[0]
-        with open("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/metappisp_bound_k_2_clustering.csv") as infile_2:
+        with open("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/metappisp_bound_k_2_clustering_and_fscore.csv") as infile_2:
             for item in infile_2:
                 protein_infile_2 = item.strip().split(",")[0]
-                rest_of_line = ",".join(item.strip().split(",")[1:6])
+                rest_of_line = ",".join(item.strip().split(",")[1:7])
                 if protein_infile == protein_infile_2:
-                    with open ("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/bound_compiled.csv", "a") as outfile:
+                    with open ("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/clustering_bound_results/bound_fscore_compiled.csv", "a") as outfile:
                         outfile.write(f"{line.strip()},{rest_of_line}\n")
                 
 
