516bf23d6945e1586f06c1933a6b5c9184eb526e
mcarroll8 <moshecarroll8@gmail.com>
2021-11-20 20:58:06 -0500

    commit

--- Spidder/Scripts/spidder_download.py
+++ Spidder/Scripts/spidder_download.py
@@ -1,7 +1,10 @@
-file = "http://polyview.cchmc.org/cgi-bin/get_data.cgi?FN=595253&FT=3"
+#interate over links
+
+link = "http://polyview.cchmc.org/cgi-bin/get_data.cgi?FN=595253&FT=3"
 import os, sys
 import requests
 
-url_txt = f"{file}"
+url_txt = f"{link}"
 r_txt = requests.get(url_txt, allow_redirects=True)
-open(f"/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/Spidder/output_pdb/pdb_file_download_test.txt", 'wb').write(r_txt.content) #fix path
+
+open(f"#insert file path here, rename each file to correlate with the pdb name", 'wb').write(r_txt.content) #fix path
