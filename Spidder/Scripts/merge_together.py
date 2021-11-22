9247754b0ac1a91f6d50facc595b26f960941cf3
mcarroll8 <moshecarroll8@gmail.com>
2021-11-21 22:11:49 -0500

    commit

---
+++ Spidder/Scripts/merge_together.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+
+unbound_folder = Path("/Users/moshe/Desktop/Research_Antigen/Vorffip/Unbound_work/Data/unbound_pdbs_reformatted")
+for file in unbound_folder.iterdir():
+    with open(file) as unbound_file:
+        protein_name1 = file.name[:4]
+        protein_name2 = file.name[7:11]
+        protein_name = f"{protein_name1}_{protein_name2}"
+        print(protein_name)
+        for line in unbound_file:
+            unbound_res = line.split("_")[0]
+            #fix path
+
+            annotated_folder = Path("/Users/moshe/Desktop/Research_Antigen/Updated_PDBs_and_Data/Unbound_annotated_or_not_for_residues")
+            
+            for item in annotated_folder.iterdir():
+                ann_protein_name = item.name[:9]
+                if ann_protein_name == protein_name:
+                    with open(item) as annotated_file:
+                        for line1 in annotated_file:
+                         
+                            annotated_res_num = line1.split("_")[0]
+                            if_annotated = line1.split(",")[2]
+                            if annotated_res_num== unbound_res:
+                                #fix path
+                                with open()
+                                    f.write(f"{line.strip()},{if_annotated.strip()}\n")
+
+
+
+#fix path
+next_folder = Path()
+for input_file in next_folder.iterdir():
+
+    with open(input_file, "r") as fp:
+        lines = fp.readlines()
+        new_lines = []
+        for line in lines:
+            #- Strip white spaces
+            line = line.strip()
+            first_half = line.strip().split(",")[0].split("_")[0]
+            first_halfa = line.strip().split(",")[0].split("_")[1]
+            first_half_complete = f"{first_half}_{first_halfa}"
+            #print(first_half_complete)
+            # first_half_2b = first_half_2.split(",")[0].split("_")[1]
+            # first_half_2_complete = f"{first_half_2a}_{first_half_2b}"
+            # print(first_half_2_complete)
+            
+            if line not in new_lines:
+                str1 = ""
+                if first_half_complete not in "\n".join(new_lines):
+                    new_lines.append(line)
+    #print("\n".join(new_lines))
+    #fix path
+    with open(), "w") as fp:
+        fp.write("\n".join(new_lines))
+
+
+
+
+
+#fix paths
+folder_2 = Path()
+with open(, "w") as outfile:
+
+    for file in folder_2.iterdir():
+    
+        with open(file) as infile:
+            contents = infile.read()
+            outfile.write(f"{contents}\n")
+
+            
+
+
+
+
+
+
+
+# annotated_unbound_folder = Path("/Users/moshe/Desktop/Research_Antigen/Updated_PDBs_and_Data/antigen_complexed/annotated_results_updated") 
+# results_folder = Path("/Users/moshe/Desktop/Research_Antigen/ispred/ispred_bound_formatted")
+# for file in results_folder.iterdir():
+    
+#     with open(file, "r") as file1:
+#         data = file1.read()
+
+#         #get number of occurrences of the substring in the string
+#         occurrences = data.count(",1")
+#         protein_name = file.name[:4]
+#         #print('Number of occurrences of the word :', occurrences, protein_name)
+#         for annotated_file in annotated_unbound_folder.iterdir():
+#             if protein_name in str(annotated_file.name):
+#                 unbound_annotated_results_file = annotated_file
+#                 with open(unbound_annotated_results_file) as file_unbound:
+#                     file_unbound = open(unbound_annotated_results_file,"r")
+                
+#                     Counter = 0
+#                     Content = file_unbound.read()
+#                     CoList = Content.split("\n")
+
+#                     for i in CoList:
+#                         if i:
+#                             Counter += 1
+#                     counter1 = Counter
+
+                
+#                 if counter1 != occurrences:
+#                     print(counter1, occurrences, protein_name)
--- Spidder/Scripts/spidder_submit.py
+++ Spidder/Scripts/spidder_submit.py
@@ -20,7 +20,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 
-folder = Path("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/unbound_data/unbound_pdbs")
+folder = Path("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/bound_data/bound_pdbs")
 for file_trying_path in folder.iterdir():
 
     browser = webdriver.Chrome(executable_path=DRIVER_PATH,
@@ -29,7 +29,7 @@ for file_trying_path in folder.iterdir():
     browser.get(site)
 
     email_input = browser.find_element_by_name("EMail")
-    email_input.send_keys("unboundspidder@gmail.com")
+    email_input.send_keys("boundspidder@gmail.com")
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 
