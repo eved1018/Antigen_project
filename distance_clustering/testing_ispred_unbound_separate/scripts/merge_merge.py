2c095ac45075639de0c2a85357edc807666dc471
mcarroll8 <moshecarroll8@gmail.com>
2022-01-13 12:43:35 -0500

    commit

---
+++ distance_clustering/testing_ispred_unbound_separate/scripts/merge_merge.py
@@ -0,0 +1,12 @@
+with open ("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/testing_ispred_unbound_separate/cluster_1_2_merge_results.csv") as infile_1:
+    for line in infile_1:
+        protein = line.strip().split(",")[0]
+        with open ("/Users/moshe/Desktop/Research_Antigen/antigen_project_updated/Antigen_project/distance_clustering/testing_ispred_unbound_separate/ispred_unbound_k_2_clustering_predicted_only.csv") as infile_2:
+            for item in infile_2:
+                protein_2 = item.strip().split(",")[0]
+                parta = item.strip().split(",")[1]
+                partb = item.strip().split(",")[2]
+                partc = item.strip().split(",")[3]
+                partd = item.strip().split(",")[4]
+                if protein_2 == protein:
+                    print(f"{line.strip()},{parta},{partb},{partc},{partd}")
